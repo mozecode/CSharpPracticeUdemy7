6c8a189884104a90afc1a509b490b1a81a0c26cd
Greta Moseley <greta.moseley@gmail.com>
2017-12-20 11:21:18 -0600

    add C# exercise to create random number guessing game with do while loop and basic conditionals

--- CSharpUdemyPractice7/Program.cs
+++ CSharpUdemyPractice7/Program.cs
@@ -9,7 +9,32 @@ namespace CSharpUdemyPractice7
     class Program
     {
         static void Main(string[] args)
-        {
+        {   //Write a program that picks a random number between 1 and 10. Give the user 4 chances to
+            //guess the number. If the user guesses the number, display “You won"; otherwise, display “You lost". 
+            //(To make sure the program is behaving correctly, you can display the secret number on the console first.)
+
+            var random = new Random();
+            var attempts = 1;
+            int guess;
+            var numberToGuess = random.Next(1, 10);
+            Console.WriteLine(numberToGuess);
+            do
+            {
+                Console.WriteLine("Please enter a number between 1 and 10: ");
+                guess = int.Parse(Console.ReadLine());
+                if (guess != numberToGuess)
+                {
+                    Console.WriteLine("You lost.");
+                    attempts++;
+
+                }
+                else
+                {
+                    Console.WriteLine("Yay! You won.");
+                    break;
+                }
+
+            } while (attempts <= 4);
         }
     }
 }
